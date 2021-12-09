2d072377b8d6453e0643bb5d97e2cb593db615cb
Manikantahariharan <manikanta.j@cyware.com>
2021-12-09 20:26:41 +0530

    label testcases addition under the test_label

--- utilities/Actions.py
+++ utilities/Actions.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.keys import Keys
 from datetime import datetime
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 class Action:
@@ -11,11 +12,22 @@ class Action:
     def click(self, element):
         element.click()
 
+    def getcount(self, element):
+        count = element.text
+        return count.split('(')[1].split(')')[0]
+
     def sendKeys(self, inputfield, value):
         inputfield.send_keys(value)
 
+    def clear_field(self,element):
+        while len(element.get_attribute("value")) > 0:
+            element.send_keys(Keys.BACK_SPACE)
+
     def clickEnter(self, inputfield):
         inputfield.send_keys(Keys.ENTER)
+        action1 = ActionChains(self.driver)
+        action1.send_keys(Keys.ENTER)
+        action1.perform()
 
     def selectFromDD(self, dropdown, value):
         ddelement = Select(dropdown)
