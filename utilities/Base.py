03a1087cde8e9e42bb3c095c7296a9f29de08e00
Sandeep kumar <sandeep.kumar@cyware.com>
2021-12-14 19:16:26 +0530

    base changes and .gitignore update

--- utilities/Base.py
+++ utilities/Base.py
@@ -11,8 +11,16 @@ class Base:
             loggerName = inspect.stack()[1][3]
             logger = logging.getLogger(loggerName)
             current_folder = os.path.dirname(os.path.abspath(__file__))
-            log_path = os.path.join(current_folder, "../", 'logs', 'automation.log')
-            filehandler = logging.FileHandler(log_path)
+            log_path = os.path.join(current_folder, "../", 'logs')
+            # Check whether the specified path exists or not
+            isExist = os.path.exists(log_path)
+            if not isExist:
+                # Create a new directory because it does not exist
+                os.makedirs(log_path)
+                print("logs directory is created!")
+                #log_file = open("logfile.log", "w+")
+            final_log_path = os.path.join(current_folder, "../", 'logs', 'logfile.log')
+            filehandler = logging.FileHandler(final_log_path)
             formatter = logging.Formatter("%(asctime)s : %(levelname)s : %(name)s : %(message)s")
             filehandler.setFormatter(formatter)
             logger.addHandler(filehandler)
