1432d4bb998be584568d6ee87470942ecb852903
Yugandhara Gaikwad <yugandhara.g@cyware.com>
2021-12-23 06:11:11 +0000

    entrypoint.sh edited online with Bitbucket

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 ./venv_setup_mac.sh
 source venv/bin/activate
-py.test -s -v -m smoke --html=./reports/report.html --capture sys
+py.test -s -v -m $marker --html=./reports/report.html --capture sys
