c28c27adc76f3bc90bdcd0ddc0f7078c830907ca
Sandeep kumar <sandeep.kumar@cyware.com>
2022-01-13 21:45:19 +0530

    Changes for docker file enterypoint 2

--- report_run.sh
+++ report_run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-py.test -s -v -m "readOnly" --url="https://csol-freemium.cywareqa.com/soar" --email="sandeep.kumar@cyware.com" --password="Cywarelabs@2022" --html=./reports/report.html --capture sys
+py.test -s -v -m readOnly --url="https://csol-freemium.cywareqa.com/soar" --email="sandeep.kumar@cyware.com" --password="Cywarelabs@2022" --html=./reports/report.html --capture sys
