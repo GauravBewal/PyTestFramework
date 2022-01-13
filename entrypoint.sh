a7e375172420da092b30d056eb2061c04291b5ce
Sandeep kumar <sandeep.kumar@cyware.com>
2022-01-13 21:11:10 +0530

    Changes for docker file enterypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,9 @@
 
 ./venv_setup_mac.sh
 source venv/bin/activate
+echo $marker
+echo $URL
+echo $Email
+echo $Password
 py.test -s -v -m $marker --url=$URL --email=$Email --password=$Password --html=./reports/report.html --capture sys
 echo "Test Execution Completed!"
