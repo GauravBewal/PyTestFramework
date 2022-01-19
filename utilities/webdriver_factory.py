d5925cef4994fbf5ca059b09895b6f62b5940e79
Sandeep Kumar <sandeep.kumar@cyware.com>
2022-01-19 15:23:37 +0000

    webdriver_factory.py edited online with Bitbucket

--- utilities/webdriver_factory.py
+++ utilities/webdriver_factory.py
@@ -62,7 +62,7 @@ class WebDriverFactory:
         else:
             driver = webdriver.Edge(EdgeChromiumDriverManager().install())
         # Setting Driver Implicit Time out for An Element
-        driver.implicitly_wait(10)
+        driver.implicitly_wait(40)
         # Maximize the window
         driver.maximize_window()
         # Loading browser with App URL
