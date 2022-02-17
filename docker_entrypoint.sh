858cb5a29b0fa8781ce503b6fd257159193c8a71
“Depinder” <Depinder.bharti@cyware.com>
2022-02-17 20:25:00 +0530

    report

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -12,6 +12,6 @@ echo "$Email"
 echo "$Password"
 py.test -s -v -m $marker --url=$URL --email=$Email --password=$Password --html=./reports/report.html --junitxml=./reports/result.xml --capture sys
 echo "Test Execution Completed!"
-python3 jenkins/xml_to_simplified_html.py
+python3 jenkins/xml_to_simplified_html.py -n reports/simplified_html.html -r reports
 
 #[ -f jenkins/bridge.sh ] && chmod +x jenkins/bridge.sh && . jenkins/bridge.sh;
