cd0eeac6caf2b6d748ef354ec2cd1a8952c55ec5
Likhith R <likhith.r@ip-192-168-137-68.ap-south-1.compute.internal>
2022-03-15 18:43:33 +0530

    Addded search, Update Webhook Name, Dropdown Edit option, Dropdown copy token testcases.

--- testCases/test_Webhooks.py
+++ testCases/test_Webhooks.py
@@ -18,6 +18,7 @@ class TestWebhooks(Base):
             Validation - 1. On the basis of Window's title
             TC_ID : 001
         """
+        global active_count
         log = self.getlogger()
         webhook = Webhooks(self.driver)
         action = Action(self.driver)
@@ -26,6 +27,7 @@ class TestWebhooks(Base):
         nav.click_admin_menu()
         log.info("Click on Webhooks tab from Admin Page")
         webhook.click_webhooks()
+        active_count = webhook.get_webhook_count()
         error_msg_visibility = nav.verify_error_msg_after_navigation()
         assert action.get_title() in 'Webhooks | Cyware Orchestrate' and error_msg_visibility is False
 
@@ -165,35 +167,92 @@ class TestWebhooks(Base):
         baseurl = ReadConfig.getAppURL()+"api/webhooks_auth/events/"
         assert baseurl == webhook.get_base_webhook_url()
 
-    # @pytest.mark.regression
-    # def test_09_search_webhook(self):
-    #     """
-    #          Verify the search bar functionality
-    #          Validation -1 : Compare the result with the searched/Updated webhook
-    #          TC_ID : 009
-    #     """
-    #     webhook = Webhooks(self.driver)
-    #     log = self.getlogger()
-    #     webhook.search_input_string(webhook_title)
-    #     webhook.click_confirm()
-    #     searched_name = webhook.get_first_webhhook_name()
-    #     webhook.clear_input_field()
-    #     assert searched_name == webhook_title
-    #
-    # @pytest.mark.regression
-    # def test_10_update_webhook_name(self):
-    #     webhook = Webhooks(self.driver)
-    #     log = self.getlogger()
-    #     action = Action(self.driver)
-    #     webhook.click_inactive_tab()
-    #     webhook.click_on_first_webhook()
-    #     webhook.clear_webhook_title()
-    #     updated_webhook = "test_webhook_"+action.get_current_time()
-    #     webhook.put_webhook_title(updated_webhook)
-    #     webhook.update_webhook()
-    #     webhook.search_input_string(updated_webhook)
-    #     webhook.click_confirm()
-    #     assert updated_webhook == webhook.get_first_webhhook_name()
+    @pytest.mark.regression
+    def test_09_search_webhook(self):
+        """
+             Verify the search bar functionality
+             Validation -1 : Compare the result with the searched/Updated webhook
+             TC_ID : 009
+        """
+        webhook = Webhooks(self.driver)
+        log = self.getlogger()
+        log.info("Verifying the searching of the created Webhook")
+        log.info("Provide input for the search bar")
+        webhook.search_input_string(webhook_title)
+        log.info("Click Enter/Confirm")
+        webhook.click_confirm()
+        log.info("Get the first/ Name of the searched webhook")
+        searched_name = webhook.get_first_webhhook_name()
+        log.info("Clear the input/search bar")
+        webhook.clear_input_field()
+        log.info("Assert based on the input to search bar and the webhook title that result shown.")
+        assert searched_name == webhook_title
+
+    @pytest.mark.regression
+    def test_10_update_webhook_name(self):
+        """
+            Update New Webhook Name
+            Validation -1 : Comparing the Name the updated Webhook Name.
+            TC_ID : 10
+        """
+        webhook = Webhooks(self.driver)
+        log = self.getlogger()
+        action = Action(self.driver)
+        webhook.click_inactive_tab()
+        log.info("Click on the first Webhook in list")
+        webhook.click_on_first_webhook()
+        log.info("Clear the old name of Webhook")
+        webhook.clear_webhook_title()
+        log.info("Generating the New Webhook Name")
+        updated_webhook = "test_webhook_"+action.get_current_time()
+        log.info("Update a New Name to Webhook")
+        webhook.put_webhook_title(updated_webhook)
+        log.info("Click Update/Save Button")
+        webhook.update_webhook()
+        log.info("Search the new updated Webhook Name")
+        webhook.search_input_string(updated_webhook)
+        webhook.click_confirm()
+        assert updated_webhook == webhook.get_first_webhhook_name()
+
+    @pytest.mark.regression
+    def test_11_check_the_dropdown_Edit_visibility(self):
+        """
+            Verify the dropdown button Visibility.
+            Validation -1 : Check The Edit Slider Title
+            TC_ID:11
+        """
+        webhook = Webhooks(self.driver)
+        log = self.getlogger()
+        log.info("Mouse over the dropdown")
+        webhook.check_drop_down()
+        log.info("Click on Edit button in Drop down")
+        webhook.click_edit_button()
+        log.info("Get the Slider Name")
+        slider_title = webhook.get_slider_title()
+        log.info("Close the Slider")
+        webhook.click_slider_close()
+        assert slider_title == "Edit Webhook"
+
+    @pytest.mark.regression
+    def test_12_copy_token(self):
+        """
+            Verify user is able to copy the token
+            Validation -1 : Verify by the notification status
+            TC_ID :12
+        """
+        webhook = Webhooks(self.driver)
+        log = self.getlogger()
+        log.info("Mouse Over the Drop down")
+        webhook.check_drop_down()
+        log.info("Click on the Copy token Button in Drop down")
+        webhook.click_copy_token_button()
+        log.info("Get the toast message Information")
+        toast_message = webhook.check_success_copy_token()
+        assert toast_message is True
+
+
+
+
 
 
 
