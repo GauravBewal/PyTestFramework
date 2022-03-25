fe91d32d94bed6d1a027fd8440441155f0d6421d
Utsav Vishwa <utsavvishwa@ip-192-168-29-237.ap-south-1.compute.internal>
2022-03-25 16:12:53 +0530

    Tenant Management test cases added

--- pageObjects/TenantManagement.py
+++ pageObjects/TenantManagement.py
@@ -64,3 +64,52 @@ class TenantManagement(Action):
 
     def click_slider_close(self):
         return Action.javascript_click(self, By.XPATH, TenantManagement.btn_slider_close)
+
+    tenant_count = "//h1[contains(text(),'Tenant Management (')]"
+
+    def get_count_of_tenants(self):
+        return Action.get_count_from_string(self, By.XPATH, TenantManagement.tenant_count)
+
+    tenant_name = "(//div[contains(@class,'cy-input ')]/input)[1]"
+
+    def put_tenant_name(self, value):
+        return Action.send_keys(self, By.XPATH, TenantManagement.tenant_name, value)
+
+    domain_name = "(//div[contains(@class,'cy-input ')]/input)[2]"
+
+    def put_domain_name(self, value):
+        return Action.send_keys(self, By.XPATH, TenantManagement.domain_name, value)
+
+    Save_btn = "//div[contains(@class,'add-tenant')]//button[text()='Save']"
+
+    def click_save_btn(self):
+        return Action.wait_and_click(self, By.XPATH, TenantManagement.Save_btn)
+
+    close_tooltip_btn = "//div[contains(@class,'notification__closeBtn')]"
+
+    def click_close_tooltip(self):
+        return Action.normal_click(self, By.XPATH, TenantManagement.close_tooltip_btn)
+
+    toast_msg_txt = "//div[@role='alert']//span[2]/span[1]"
+
+    def get_tooltip_msg(self):
+        return Action.get_text(self, By.XPATH, TenantManagement.toast_msg_txt)
+
+    continue_using_co_btn = "//div[contains(@class,'configuring')]//button"
+
+    def click_continue_using_co_btn(self):
+        return Action.wait_and_click(self, By.XPATH, TenantManagement.continue_using_co_btn)
+
+    main_input = "//input[@id='main-input']"
+
+    def search_input_string(self, value):
+        return Action.send_keys(self, By.XPATH, TenantManagement.main_input, value)
+
+    def click_enter_for_search(self):
+        return Action.click_enter(self)
+
+    first_tenant_name = "(//tr[@class='el-table__row']//a)[1]"
+
+    def get_name_first_tenant(self):
+        return Action.get_text(self, By.XPATH, TenantManagement.first_tenant_name)
+
